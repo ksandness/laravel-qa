d25558e56f3f4153b8e16e997c80f624c896d699
Kevin Sandness <ksandness@gmail.com>
2018-09-15 10:26:40 -0700

    Add author info and question creation date in question item

--- app/User.php
+++ app/User.php
@@ -30,4 +30,9 @@ class User extends Authenticatable
     public function questions() {
         return $this->hasMany(Question::class);
     }
+
+    public function getUrlAttribute() {
+        // return route("questions.show", $this->id);
+        return '#';
+    }
 }
